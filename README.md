4b4fbe99e12632ab2222543eb5eefdfb12f8562a
EssiLaajala <essi.laajala@gmail.com>
2021-05-10 16:08:13 +0300

    Modified readme and Step0_preprocessing

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # RRBS workflow
 
-This repository depicts an example pipeline on how to process RRBS data.
+This is an example bisulfite sequencing data analysis workflow, starting from quality control and alignment steps for .fastq files. We worked with reduced representation bisulfite sequencing (RRBS) data but most steps are applicable to WGBS as well. The anonymized RRBS data will be available in Array Express, as soon as the manuscripts are accepted for publication.
+
+## Usage
+
+This repository is intended as:
+
+1. Documentation of the data preprocessing and analysis steps of these two manuscripts:
+Laajala et al. Permutation-based significance analysis reduces the type 1 error rate in bisulfite sequencing data analysis of human umbilical cord blood samples
+Laajala et al. Umbilical cord blood DNA methylation in children who later develop type 1 diabetes
+
+2. A resource for other people who analyze bisulfite sequencing data and are fluent in R. 
 
 ## Citation
 
-Foo et al.
+Laajala et al. Permutation-based significance analysis reduces the type 1 error rate in bisulfite sequencing data analysis of human umbilical cord blood samples
+
+Currently the paper is not yet citable but we are submitting it to bioRxiv and a peer-reviewed journal asap. Right now you can cite this repository: https://github.com/EssiLaajala/RRBS_workflow.git
+
+
--- Step0_preprocessing/M_bias_example/Code/Example_detect_samples_that_need_trimming.R
+++ Step0_preprocessing/M_bias_example/Code/Example_detect_samples_that_need_trimming.R
@@ -3,12 +3,13 @@
 # I trimmed everything that differed from the methylation levels of middle positions by more than 3 sd (Read 1 3', Read 2 5' and Read 2 3')
 # This is an example of reading M bias files and printing the names of those samples that need further trimming
 # This is only intended as a documentation of what was done and is not a nice script for others to use. 
-# If you want to use it, you need to look at the M bias files first to know how many lines to skip and how many to read (nrows)
+# If you want to use it, you need to look at the M bias files first to know how many lines to skip and how many to read (read length)
 # This doesn't work unless all M-bias files in the working directory are for the same read length
 
 ## Choose these:
 # Working directory: It should contain the M-bias files.
-# files <- list.files(pattern="txt") # files should now contain M-bias file names, one file for each sample (in this example the working directory contains no other txt files)
+files <- # list.files(pattern="txt") 
+# files should now contain M-bias file names, one file for each sample (in this example the working directory contains no other .txt files)
 sd_limit <- # How many standard deviations away from the mean is considered extreme? I've used value 3.
 middle <- # I used 10:90 (middle 80 % of read length 100)
 nbr_rows_to_skip <- # For example, if you want to look at read 2 CpG M-bias in the example file, skip 318 rows
@@ -31,10 +32,10 @@ for(i in files){
 	if(sum(above_normal_variation, na.rm=T) + sum(below_normal_variation, na.rm=T) > 0) to_be_trimmed <- c(to_be_trimmed,i)
 }
 to_be_trimmed
-# This just printed M-bias file names that indicate more trimming is needed. How much? Try different start_positions. In this example, if you start from the 4th base, this prints an empty vector indicating that bases 4:9 are within the normal variation. It's enough to trim away the first base
+# This just printed M-bias file names that indicate more trimming is needed. How much? Try different start_positions. In this example, if you start from the 4th base, this prints an empty vector indicating that bases 4:9 are within the normal variation. It's enough to trim away the first three bases.
 # For an individual sample, the vectors above_normal_variation and below_normal_variation will also tell you where to trim. 
 
-# This example sample had a value outside normal variation at position 3. Other samples had them at positions 1 or 2 (of read 2). For simplicity, first 3 bases were trimmed from the 5' end of read 2 of all samples. The other option is to do different trimmings for different samples.
+# This example sample had values outside normal variation at positions 1-3. Other samples had them only at position 1 or 2 (of read 2). For simplicity, first 3 bases were trimmed from the 5' end of read 2 of all samples. The other option is to do different trimmings for different samples.
 
 ###########
 # End
@@ -50,8 +51,8 @@ for(i in files){
 	if(sum(above_normal_variation, na.rm=T) + sum(below_normal_variation, na.rm=T) > 0) to_be_trimmed <- c(to_be_trimmed,i)
 }
 to_be_trimmed
-# In this example it's enough to trim away one base. If you try end_positions = 91:98, it prints an empty vector
+# In this example it's enough to trim away one base. If you try end_positions <- 91:98, it prints an empty vector
 
-# All samples had the same pattern in this project: One outlier at the last base of read 2.
+# One base was trimmed from the 3' end of read 2 of all samples (in this project).
 
 
